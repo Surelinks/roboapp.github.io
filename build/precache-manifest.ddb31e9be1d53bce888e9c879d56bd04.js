9e208285014eb4d20024970148ee97d6189ab3ca
surelinks <fumsamuel@gmail.com>
2019-05-17 22:23:37 +0100

    Built file
    initial

---
+++ build/precache-manifest.ddb31e9be1d53bce888e9c879d56bd04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd75cde24d74766df868c1fc1542b796",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "072f80f35c14cf47064a",
+    "url": "/static/css/main.238a7fee.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "072f80f35c14cf47064a",
+    "url": "/static/js/main.75cc021e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
